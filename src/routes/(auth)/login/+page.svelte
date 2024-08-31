b74682ae01be0b691e5165af557e036a6e4cb178
Jhinger <gurshanjhinger22@gmail.com>
2024-08-30 21:14:42 -0700

    feat: login landing page, and setup

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { Button } from '$components/ui/button/index.js';
+	import { Input } from '$components/ui/input/index.js';
+	import { Label } from '$components/ui/label/index.js';
+</script>
+
+<div class="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
+	<div class="flex items-center justify-center py-12">
+		<div class="mx-auto grid w-[350px] gap-6">
+			<div class="grid gap-2 text-center">
+				<h1 class="text-3xl font-bold">Login</h1>
+				<p class="text-muted-foreground text-balance">
+					Enter your email below to login to your account
+				</p>
+			</div>
+			<div class="grid gap-4">
+				<div class="grid gap-2">
+					<Label for="email">Email</Label>
+					<Input id="email" type="email" placeholder="email@example.com" required />
+				</div>
+				<div class="grid gap-2">
+					<div class="flex items-center">
+						<Label for="password">Password</Label>
+						<a href="##" class="ml-auto inline-block text-sm underline"> Forgot your password? </a>
+					</div>
+					<Input id="password" type="password" required />
+				</div>
+				<Button type="submit" class="w-full">Login</Button>
+				<Button variant="outline" class="w-full">Login with Google</Button>
+			</div>
+			<div class="mt-4 text-center text-sm">
+				Don&apos;t have an account?
+				<a href="##" class="underline"> Sign up </a>
+			</div>
+		</div>
+	</div>
+	<div class="relative bg-muted hidden lg:block">
+		<enhanced:img
+			src="$lib/images/login-stock.jpg"
+			alt="placeholder"
+			fetchpriority="high"
+			loading="eager"
+			class="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+		/>
+		<div class="absolute top-5 left-10">
+			<span class="text-2xl text-white font-gochi font-black">ByteBite</span>
+		</div>
+	</div>
+</div>
